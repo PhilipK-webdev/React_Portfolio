617204943f2454a38e8d0998e167e55e22c29f94
Philip Kouchner <philipk179@gmail.com>
2020-08-20 19:41:18 -0700

    workig on the routes

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import Header from './component/Header';
 import About from './component/About'
 import Portfolio from './component/Portfolio';
 import Home from './component/Home';
-
+import images from './images/MySelf.jpg'
 
 function App() {
+  const title = "Kouchner Philip";
+  const subheader = "Full Stack Web Developer";
+  const img = images;
+  const bio = "Currently studying at Berkley Extension as Full-Stack Web Developer.Looking for my new challenge in frontend, backend or Both. I'm a fast learner with excellent background in computers I like to learn new technologies and expand my knowledge in a variety of fields";
+  const avatar = "😎";
   return (
     <div className="App" >
       <Router>
         <Header />
         <Switch>
+          <Home title={title} subheader={subheader} img={img} bio={bio} avatar={avatar} />
           <Route exact path="/" component={Home} />
           <Route exact path="/portfolio" component={Portfolio} />
           <Route exact path="/about" component={About} />
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,16 +1,8 @@
 import React from 'react'
 import { Grid } from "@material-ui/core";
 import CardHome from './CardHome';
-import images from '../images/MySelf.jpg'
 
-
-const Home = () => {
-
-    const title = "Kouchner Philip";
-    const subheader = "Full Stack Web Developer";
-    const img = images;
-    const bio = "Currently studying at Berkley Extension as Full-Stack Web Developer.Looking for my new challenge in frontend, backend or Both. I'm a fast learner with excellent background in computers I like to learn new technologies and expand my knowledge in a variety of fields";
-    const avatar = "😎";
+const Home = ({ title, subheader, img, bio, avatar }) => {
     return (
         <Grid container direction="column">
             <Grid item container>
