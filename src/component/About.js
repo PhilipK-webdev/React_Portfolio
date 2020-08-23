e474667172b6f7d63c02a857fc944a3441e0903e
Philip Kouchner <philipk179@gmail.com>
2020-08-23 09:19:07 -0700

    finish the about page

--- src/component/About.js
+++ src/component/About.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Container, Card } from '@material-ui/core';
+import Link from '@material-ui/core/Link';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import MailIcon from '@material-ui/icons/Mail';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
 
 const About = () => {
+
+    const useStyles = makeStyles({
+        root: {
+            display: 'flex',
+            justifyContent: "center",
+            alignContent: "center",
+            marginTop: "50px",
+            marginLeft: "40px",
+            marginBottom: "50px"
+        },
+        card: {
+
+            // height: "290px",
+            width: "250px",
+            marginTop: "10px",
+        }
+    });
+
+    const classes = useStyles();
+
+
+
     return (
-        <div>
-            <h1>Hello</h1>
-        </div>
-    )
+        <Container maxWidth="sm" >
+            <Card className={classes.root} >
+                <div className={classes.root}>
+                    <Link href="https://www.linkedin.com/in/philip-kouchner-886264100/">
+                        <LinkedInIcon fontSize={"large"} />
+                    </Link>
+                    <Link href="mailto:philipk179@gmail.com" label={"test@example.com"}>
+                        <MailIcon fontSize={"large"} />
+                    </Link>
+                    <Link href="https://github.com/PhilipK-webdev" >
+                        <GitHubIcon fontSize={"large"} />
+                    </Link>
+                </div>
+            </Card>
+
+        </Container>
+
+    );
 }
 
 export default About
+
